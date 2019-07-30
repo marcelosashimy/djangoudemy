d3afe4142f63a6d5f5dbf0ca2c8c757bfb0e8535
marcelosashimy <marcelo.sashimy@hotmail.com>
2019-07-30 15:57:53 -0300

    formularios

--- simplemooc/simplemooc/courses/urls.py
+++ simplemooc/simplemooc/courses/urls.py
@@ -2,12 +2,14 @@
 from django.urls import path
 from simplemooc.courses.views import courses , details
 
+#coloca sempre o nome do APP, para identificar de onde irá vim a url.
+#Em outros lugares ficará por conta disso assim:  "APP":"URL" . EX: courses:details
 
-
+app_name = 'courses'
 urlpatterns = [
 
-    path('', courses,name='courses'),
+    path('', courses, name='courses'),
     #path(r'(?P)<pk>\d+)',details, name='details')
-    path('<int:pk>/', details, name='details'),
-
+    #path('<int:pk>/', details, name='details'),
+    path('<slug:slug>/', details, name='details'),
 ]
--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 #importando o modelo
 from .models import Course
+from .forms import ContactCourse
 
 #responsavel por renderizar o template da listagem dos cursos
 def courses(request):
@@ -14,14 +15,31 @@ def courses(request):
 	return render(request, template_name, context)
 
 
-def details(request, pk):
+#def details(request, pk):
 	#course= Course.objects.get(pk=pk)
 	#Course é uma model
-	course=get_object_or_404(Course, pk=pk)
+#	course=get_object_or_404(Course, pk=pk)
+#	context = {
+#		'course':course
+#	}
+#	template_name = 'courses/details.html'
+#	return render(request, template_name, context)
+
+
+def details(request, slug):
+	#course= Course.objects.get(pk=pk)
+	#Course é uma model
+	course=get_object_or_404(Course, slug=slug)
+	if request.method =='POST':
+		form=ContactCourse(request.POST)
+	else:
+		form=ContactCourse()
 	context = {
-		'course':course
+		'course':course,
+		'form':form
 	}
 	template_name = 'courses/details.html'
 	return render(request, template_name, context)
 
 
+		
